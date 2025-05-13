dfbd28ee9d7f92a37f32d9ece68246a89fd37838
Alexander-Bartmann <seirex.mb@gmail.com>
2025-05-13 22:04:38 +0200

    Add new image assets for Angular, CSS, Firebase, Git, Location, React, TypeScript, and Vue
    - Added angular.png to the public/img directory.
    - Added css.png to the public/img directory.
    - Added firebase.png to the public/img directory.
    - Added git.png to the public/img directory.
    - Added location.png to the public/img directory.
    - Added react.png to the public/img directory.
    - Added typescript.png to the public/img directory.
    - Added vue.png to the public/img directory.

--- src/app/main/my-skills/my-skills.component.ts
+++ src/app/main/my-skills/my-skills.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-my-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-skills.component.html',
   styleUrl: './my-skills.component.scss'
 })
 export class MySkillsComponent {
-
+  skills = [
+    { name: 'Angular', img: 'img/angular.png' },
+    { name: 'TypeScript', img: 'img/typescript.png' },
+    { name: 'JavaScript', img: 'img/Javascript.png' },
+    { name: 'HTML5', img: 'img/html.png' },
+    { name: 'CSS', img: 'img/css.png' },
+    { name: 'Firebase', img: 'img/firebase.png' },
+    { name: 'Git', img: 'img/git.png' },
+    { name: 'Scrum', img: 'img/scrum.png' }
+  ];
 }
