ee2919c043266f075239a8424bdd45b9db498da0
Alexander-Bartmann <seirex.mb@gmail.com>
2025-10-09 15:04:54 +0200

    Refactor hero component layout and styles for improved responsiveness and structure; add menu toggle functionality

--- src/app/main/hero/hero.component.ts
+++ src/app/main/hero/hero.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule , MatIconModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
 export class HeroComponent {
+  menuOpen = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
