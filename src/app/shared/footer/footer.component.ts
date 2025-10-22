45c399d07fc1f0ae5a6ee2832b6a6b41783ec1a8
Alexander-Bartmann <seirex.mb@gmail.com>
2025-10-22 17:08:51 +0200

    Add legal notice and privacy policy components; update footer to include legal notice link and translations

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { TranslateModule } from '@ngx-translate/core';
+import { LegalNoticeComponent } from '../legal-notice/legal-notice.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [TranslateModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  constructor(private dialog: MatDialog) {}
 
+  openLegalNotice() {
+    this.dialog.open(LegalNoticeComponent, {
+    });
+  }
 }
