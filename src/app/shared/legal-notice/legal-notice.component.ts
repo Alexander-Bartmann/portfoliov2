f46409c30115f1529cccbf77ad660f39a10423e7
Alexander-Bartmann <seirex.mb@gmail.com>
2025-10-24 10:29:00 +0200

    Enhance legal notice component; add close icon functionality, improve styling, and adjust layout for mobile responsiveness

--- src/app/shared/legal-notice/legal-notice.component.ts
+++ src/app/shared/legal-notice/legal-notice.component.ts
@@ -2,14 +2,17 @@ import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { FooterComponent } from "../footer/footer.component";
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [TranslateModule, NavbarComponent, FooterComponent],
+  imports: [TranslateModule, NavbarComponent, FooterComponent, MatIcon],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
 export class LegalNoticeComponent {
-
+  close(): void {
+    window.history.back();
+  }
 }
